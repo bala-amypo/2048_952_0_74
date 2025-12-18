8701c9df42cfe1ac40e4054cf8c4426986c1cf5f
bala-amypo <bala@amypo.in>
2025-12-18 05:29:56 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,14 +1,10 @@
 package com.example.demo.controller;
+
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
@@ -16,22 +12,27 @@ import com.example.demo.service.StudentService;
 @RestController
 @RequestMapping("/student")
 public class StudentController {
-    @Autowired 
-    StudentService stdser;
-    @PostMapping("/addStudent")
-    public Student addStudent(@RequestBody Student st){
+
+    @Autowired
+    private StudentService stdser;
+
+    @PostMapping("/add")
+    public Student addStudent(@RequestBody Student st) {
         return stdser.poststudent(st);
     }
-    @GetMapping("/getall")
-    public List<Student> get(){
+
+    @GetMapping("/all")
+    public List<Student> getAll() {
         return stdser.getAllStudents();
     }
-    @GetMapping("/getById/{id}")
-    public Optional<Student> getId(@PathVariable Long id){
-     return stdser.getById(id);
+
+    @GetMapping("/{id}")
+    public Optional<Student> getById(@PathVariable Long id) {
+        return stdser.getById(id);
     }
+
     @PutMapping("/update/{id}")
-    public String update(@PathVariable Long id, @RequestBody Student st){
-        return stdser.updateData(id,st);
+    public Student update(@PathVariable Long id, @RequestBody Student st) {
+        return stdser.updateData(id, st);
     }
 }
